3c7f08191cf1dc1e1b7e15208c755ff186d348bd
Alan <alaurit@gmail.com>
2016-08-02 11:24:34 -0500

    first commit

---
+++ Tutorial01/src/Application.java
@@ -0,0 +1,27 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		int myNumber = 88;
+		short myShort = 847;
+		long myLong = 12385;
+		
+		double myDouble = 5.93863;
+		float myFloat = 4355.8f;
+		
+		char myChar = 'y';
+		boolean myBool = true;
+		
+		byte myByte = -127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBool);
+		System.out.println(myByte);
+	}
+
+}
---
+++ Tutorial3/src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+	public static void main(String[] args) {
+		
+		String hello = "Hello";
+		
+		String blank = " ";
+		
+		String name = "you";
+		
+		String greeting = hello + blank + name + "!";
+		
+		System.out.println(greeting);
+	}
+}
