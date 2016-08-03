e5c70f0d0eaee5b8221769d0b19c69bd846d425d
Alan <alaurit@gmail.com>
2016-08-03 16:08:48 -0500

    testing things while doing sololearn lessons

---
+++ sololearn1/src/sololearn1/application.java
@@ -0,0 +1,15 @@
+package sololearn1;
+
+public class application {
+
+	public static void main(String[] args) {
+		float value = 23;
+		float res = value % 5;
+		System.out.println(res);
+		
+		int test = 5;
+		test++;
+		System.out.println(test);
+	}
+
+}
---
+++ sololearn2/src/sololearn2/application.java
@@ -0,0 +1,18 @@
+package sololearn2;
+
+import java.lang.*;
+
+public class application {
+	
+	public static void main(String[] args) {
+		int n = 18; 
+		int diff = Math.abs(21 - n);
+	   if (n > 21) {
+	     diff = 2*diff;
+	   }
+	   System.out.println(diff);
+		
+
+	}
+
+}
