74ce106853e1fa7aedafa336dd95e30207394d01
Alan <alaurit@gmail.com>
2016-08-05 13:33:13 -0500

    refactored to enhanced (foreach) loop for array

--- sololearn3/src/sololearn3/application.java
+++ sololearn3/src/sololearn3/application.java
@@ -4,11 +4,11 @@ public class application {
 
 	public static void main(String[] args) {
 		int[] arr = {42, 9, 7, 22, -9};
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i]%2==0) {
+		for (int i: arr) {
+			if (i % 2 == 0) {
 				continue;
 			}
-			System.out.println(arr[i]);
+			System.out.println(i);
 		}
 		// should print 9, 7, -9
 	}
