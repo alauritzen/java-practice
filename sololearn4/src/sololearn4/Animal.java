193a963151de9580e50c7da23ed9dffe90fab8ce
Alan <alaurit@gmail.com>
2016-08-14 10:33:49 -0500

    refactored to pass animal noise and number as parameters to reuse object method

--- sololearn4/src/sololearn4/Animal.java
+++ sololearn4/src/sololearn4/Animal.java
@@ -1,7 +1,11 @@
 package sololearn4;
 
 public class Animal {
-	void bark() {
-		System.out.println("woof, woof");
+	void noise(String sound, int number) {
+		String result = "";
+		for (int i = 1; i <= number; i++) {
+			result += (sound + " ");
+		}
+		System.out.println(result);
 	}
 }
--- sololearn4/src/sololearn4/Application.java
+++ sololearn4/src/sololearn4/Application.java
@@ -3,8 +3,15 @@ package sololearn4;
 public class Application {
 
 	public static void main(String[] args) {
-		Animal dog = new Animal();
-		dog.bark();
+		Animal dog= new Animal(),
+			horse = new Animal(),
+			tiger = new Animal(),
+			cat = new Animal();
+		
+		dog.noise("woof", 3);
+		horse.noise("neigh", 1);
+		tiger.noise("growl", 2);
+		cat.noise("meow", 2);
 	}
 
 }
