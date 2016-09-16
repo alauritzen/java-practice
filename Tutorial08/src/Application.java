ead7d7fbe7d645cbeb0946020494b860e6ab9b15
Alan <alaurit@gmail.com>
2016-09-16 10:40:49 -0500

    Udemy do-while with random number

---
+++ Tutorial08/src/Application.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int count = 0;
+		
+		Random rand = new Random();
+		
+		int number = rand.nextInt(10) + 1;
+		
+		int value;
+		
+		do {
+			System.out.println("Enter a number between 1 and 10: ");
+			value = input.nextInt();
+			count ++;
+		} while (value != number);
+		
+		System.out.println("That's right! It took you " + count + " guesses!");
+		
+	}
+	
+	
+	
+	
+}
