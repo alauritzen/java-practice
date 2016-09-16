6d696eabc62df41119f7339dff246eff5a8e6be1
Alan <alaurit@gmail.com>
2016-09-16 10:48:42 -0500

    Added higher/lower prompts

--- Tutorial08/src/Application.java
+++ Tutorial08/src/Application.java
@@ -17,8 +17,17 @@ public class Application {
 		
 		do {
 			System.out.println("Enter a number between 1 and 10: ");
+			
 			value = input.nextInt();
+			
 			count ++;
+			
+			if (value > number) {
+				System.out.println("It's lower than that!");
+			} else if (value < number) {
+				System.out.println("It's higher than that!");
+			}
+			
 		} while (value != number);
 		
 		System.out.println("That's right! It took you " + count + " guesses!");
