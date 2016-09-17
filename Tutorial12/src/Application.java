6780a6283c44acc198ee8f2b5145413b3abd0766
Alan <alaurit@gmail.com>
2016-09-16 21:52:18 -0500

    Creating array, and foreach loop

---
+++ Tutorial12/src/Application.java
@@ -0,0 +1,18 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		// String[] names = new String[3];
+		
+		String[] names = { "Tom", "Dick", "Harry" };
+		
+		for (String name : names) {
+			
+			System.out.println(name);
+			
+		}
+		
+	}
+
+}
