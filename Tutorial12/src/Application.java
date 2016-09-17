3c91f97460e66d158d7365c44077b451899bb939
Alan <alaurit@gmail.com>
2016-09-16 23:24:54 -0500

    commented out nested foreach; used nested for loops

--- Tutorial12/src/Application.java
+++ Tutorial12/src/Application.java
@@ -5,11 +5,15 @@ public class Application {
 		
 		String[][] names = { { "Tom", "Dick", "Harry" }, { "Cat", "Jane", "Sally" } };
 		
-		for (String[] name1 : names) {
+		/* for (String[] name1 : names) {
 			for (String name2 : name1) {
 				System.out.println(name2);
 			}
-			
+		} */
+		
+		for (int row = 0; row < names.length; row++) {
+			for (int col = 0; col < names[row].length; col++)
+				System.out.println(names[row][col]);
 		}
 		
 	}
